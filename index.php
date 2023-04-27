2cd3090cca9b5fa236c7ab5103482d1fc74b7858
asankoh <89537452+asankoh@users.noreply.github.com>
2023-04-27 10:58:24 -0400

    Active Home Page + Styled Details Page + Login Page

--- index.php
+++ index.php
@@ -11,38 +11,9 @@ require_once("application/config.php");
 //load autoloader
 require_once("vendor/autoload.php");
 
+
+$user_controller = new UserController();
+
 //load the dispatcher that dissects a request URL
 new Dispatcher();
 
-//retrieve the action from a querystring variable named "action"
-/* if (!($action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING))) {
-    $action = "index";  //default action
-}
-$user_controller = new UserController();
-
-//take different action depending on the action value
-switch ($action) {
-    case "index":
-        $user_controller->index();
-        break;
-    case "register":
-        $user_controller->register();
-        break;
-    case "login":
-        $user_controller->login();
-        break;
-    case "verify":
-        $user_controller->verify();
-        break;
-    case "logout":
-        $user_controller->logout();
-        break;
-    case "reset":
-        $user_controller->reset();
-        break;
-    case "do_reset":
-        $user_controller->do_reset();
-        break;
-    default:
-        $user_controller->error("Invalid action was requested.");
-} */
